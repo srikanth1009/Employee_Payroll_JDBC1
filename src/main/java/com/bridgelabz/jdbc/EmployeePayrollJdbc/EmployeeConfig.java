d8a628fcda8480c43ae728e2b2bc4a05e2d320d5
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-11-12 10:51:29 +0530

    Added:Ability to create conection to database

---
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayrollJdbc/EmployeeConfig.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.jdbc.EmployeePayrollJdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class EmployeeConfig {
+
+    public Connection getConfig(){
+        String URL_JD = "jdbc:mysql://localhost:3306/payroll_services";
+        String USER_NAME = "srikanth";
+        String PASSWORD = "root";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Drivers loaded!!");
+            connection = DriverManager.getConnection(URL_JD,USER_NAME,PASSWORD);
+            System.out.println("connection Established!!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
+
---
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayrollJdbc/EmployeePayRoll.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.jdbc.EmployeePayrollJdbc;
+public class EmployeePayRoll {
+    public static void main(String[] args) {
+        EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
+    }
+}
