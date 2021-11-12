9eb6a4f470e4b290bd98eda08da7e9f998c56587
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-11-12 10:59:45 +0530

    Added:Ability to retrive data from payroll service database

---
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayrollJdbc/EmployeeException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.jdbc.EmployeePayrollJdbc;
+public class EmployeeException extends RuntimeException{
+
+    public EmployeeException(String message) {
+        super(message);
+    }
+}
